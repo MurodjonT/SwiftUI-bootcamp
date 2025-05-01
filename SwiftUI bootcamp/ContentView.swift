a0b094d59264bf4cfc4b85bf9d3efe701dda4bfa
MurodjonT <murodjon.mmm.99@mail.ru>
2025-05-01 14:26:05 +0500

    Changes frames

--- SwiftUI bootcamp/ContentView.swift
+++ SwiftUI bootcamp/ContentView.swift
@@ -1,24 +1,45 @@
+
 //
-//  ContentView.swift
-//  SwiftUI bootcamp
+//  frameBootcamp.swift
+//  SwiftUI Learning
 //
 //  Created by Murodjon Turobov on 01/05/25.
 //
 
 import SwiftUI
 
-struct ContentView: View {
+struct FrameBootcamp: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            HStack(spacing: 8) {
+                Image(systemName: "person.crop.circle")
+                    .font(.system(size: 64))
+//                    .border(Color.gray)
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Murodjon Turobov ")
+                        .lineLimit(1)
+                        .font(.system(size: 17))
+                    Text("iOS Developer")
+                        .font(.system(size: 14))
+                        .foregroundColor(.gray)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                Image(systemName: "chevron.right")
+                    .padding(.leading, 20)
+            }
+            .frame(maxWidth: .infinity, idealHeight: .infinity, alignment: .leading)
+            .padding()
+            .background(Color(.systemBackground))
+            .cornerRadius(24)
+            
         }
-        .padding()
+        .padding(.horizontal)
+        .padding(.vertical, 24)
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        .background(Color(.secondarySystemBackground))
+        
     }
 }
-
 #Preview {
-    ContentView()
+    FrameBootcamp()
 }
