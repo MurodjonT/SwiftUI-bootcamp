d432b8092704a308e588b3d6dee5717edec3f6a1
MurodjonT <murodjon.mmm.99@mail.ru>
2025-05-01 14:27:40 +0500

    name changes

--- SwiftUI bootcamp/ContentView.swift
+++ SwiftUI bootcamp/ContentView.swift
@@ -16,7 +16,7 @@ struct FrameBootcamp: View {
                     .font(.system(size: 64))
 //                    .border(Color.gray)
                 VStack(alignment: .leading, spacing: 4) {
-                    Text("Murodjon Turobov ")
+                    Text("Murodjon Turobov 12433254234")
                         .lineLimit(1)
                         .font(.system(size: 17))
                     Text("iOS Developer")
