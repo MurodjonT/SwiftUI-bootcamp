bafbcb698701fe2f293eb60a989d213e8abb2d62
MurodjonT <murodjon.mmm.99@mail.ru>
2025-05-01 14:20:30 +0500

    Initial Commit

---
+++ SwiftUI bootcamp/SwiftUI_bootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_bootcampApp.swift
+//  SwiftUI bootcamp
+//
+//  Created by Murodjon Turobov on 01/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_bootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
