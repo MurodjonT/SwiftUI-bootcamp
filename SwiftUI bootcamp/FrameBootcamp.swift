d3384f93225176e9cbe8bfc9bce40858aa813195
MurodjonT <murodjon.mmm.99@mail.ru>
2025-05-02 15:13:08 +0500

    navigation stack

--- SwiftUI bootcamp/FrameBootcamp.swift
+++ SwiftUI bootcamp/FrameBootcamp.swift
@@ -1,72 +1,56 @@
-
-//
-//  frameBootcamp.swift
-//  SwiftUI Learning
-//
-//  Created by Murodjon Turobov on 01/05/25.
-//
-
 import SwiftUI
 
 struct FrameBootcamp: View {
     var body: some View {
-        HStack {
-            HStack(spacing: 8) {
-                Image(systemName: "person.crop.circle")
-                    .font(.system(size: 64))
-                //                    .border(Color.gray)
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("Murodjon Turobov ")
-                        .lineLimit(1)
-                        .font(.system(size: 17))
-                    Text("iOS Developer")
-                        .font(.system(size: 14))
-                        .foregroundColor(.black)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                Image(systemName: "chevron.right")
-                    .padding(.leading, 20)
-            }
-            .frame(maxWidth: .infinity, idealHeight: .infinity, alignment: .leading)
-            .padding()
-            .background(Color(.systemCyan))
-            .cornerRadius(24)
-            
-        }
-        .padding(.horizontal)
-        .padding(.vertical, 24)
-        .frame(maxWidth: .infinity, alignment: .top)
-        .background(Color(.secondarySystemBackground))
-        
-        
-        HStack {
-            HStack{
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("Бонусы готовы к выплате")
-                        .lineLimit(1)
-                        .font(.system(size: 12))
-                    Text("10 400 000 cум / 150$")
-                        .font(.system(size: 18))
-                        .foregroundColor(.black)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                Image(systemName: "barcode.viewfinder")
+        NavigationStack() {
+            ScrollView {
+                HStack {
+                    HStack {
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text("Бонусы готовы к выплате")
+                                .lineLimit(1)
+                                .font(.system(size: 12))
+                            Text("10 400 000 cум / 150$")
+                                .font(.system(size: 18))
+                                .foregroundColor(.black)
+                        }
+                        .padding(-5)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        Image(systemName: "barcode.viewfinder")
+                            .padding(0)
+                            .frame(height: 74, alignment: .topTrailing)
+                            .font(.system(size: 28))
+                    }
+//                    .frame(maxWidth: .infinity, alignment: .leading)
                     .padding()
-                    .font(.system(size: 28))
+                    .background(Color(.systemCyan))
+                    .cornerRadius(24)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                .padding(.vertical, 24)                .padding(.horizontal)
+                .background(Color(.systemBackground))
+                .navigationTitle("") // Bo‘sh qoldirib o‘zingiz title qilamiz
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        HStack(spacing: 10) {
+                            Image(systemName: "person.crop.circle.fill")
+                                .resizable()
+                                .frame(width: 36, height: 36)
+                                .foregroundColor(.blue)
+                            
+                            Text("Murodjon")
+                                .font(.headline)
+                                .foregroundColor(.primary)
+                        }
+                    }
+                    
+                }
             }
-            .frame(maxWidth: .infinity, idealHeight: .infinity, alignment: .leading)
-            .padding()
-            .background(Color(.systemCyan))
-            .cornerRadius(24)
         }
-        .frame(maxWidth: .infinity, idealHeight: .infinity, alignment: .leading)
-        .padding(.horizontal)
-        .border(Color.red)
-        .background(Color(.secondarySystemBackground))
-
     }
-
 }
+
 #Preview {
     FrameBootcamp()
 }
---
+++ SwiftUI bootcamp/UserModel.swift
@@ -0,0 +1,12 @@
+//
+//  UserModel.swift
+//  SwiftUI bootcamp
+//
+//  Created by Murodjon Turobov on 01/05/25.
+//
+
+struct User {
+    let name: String
+    let role: String
+    let imageName: String // SF Symbol nomi yoki asset
+}
