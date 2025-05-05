ff0bb5bde7deb84d581cac84fb50c423ff82127c
MurodjonT <murodjon.mmm.99@mail.ru>
2025-05-05 17:47:42 +0500

    ProfileBootcamp init

---
+++ SwiftUI bootcamp/ProfileBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileBootcamp.swift
+//  SwiftUI bootcamp
+//
+//  Created by Murodjon Turobov on 05/05/25.
+//
+
+import SwiftUI
+
+struct ProfileBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileBootcamp()
+}
