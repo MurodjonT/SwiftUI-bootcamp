2de51eba192ae17075b28b3a66734e1746221a90
MurodjonT <murodjon.mmm.99@mail.ru>
2025-05-06 15:49:41 +0500

    Separated extension color

---
+++ SwiftUI bootcamp/Extension+Color.swift
@@ -0,0 +1,18 @@
+//
+//  Extension+COlor.swift
+//  SwiftUI bootcamp
+//
+//  Created by Murodjon Turobov on 06/05/25.
+//
+
+import SwiftUI
+
+struct Extension_COlor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Extension_COlor()
+}
