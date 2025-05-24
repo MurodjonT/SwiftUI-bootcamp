c39dce3dea04a2263a556a20a4d82886d5103042
MurodjonT <murodjon.mmm.99@mail.ru>
2025-05-24 19:28:08 +0500

    Segmented control update

--- SwiftUI bootcamp/TextFieldBootcamp].swift
+++ SwiftUI bootcamp/TextFieldBootcamp].swift
@@ -7,12 +7,60 @@
 
 import SwiftUI
 
+
+
+
 struct TextFieldBootcamp_: View {
+    @State private var selectedSegment: Segment = .favorites
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            CustomSegmentedControl(selectedSegment: $selectedSegment)
+
+            Text("Choosen segmented control: \(selectedSegment.rawValue)")
+                .font(.headline)
+
+            Spacer()
+        }
+        .padding()
     }
 }
 
 #Preview {
     TextFieldBootcamp_()
+}
+
+
+struct CustomSegmentedControl: View {
+    
+    @Binding var selectedSegment: Segment
+
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(Segment.allCases, id: \.self) { segment in
+                Text(segment.rawValue)
+                    .frame(maxWidth: .infinity)
+                    .fontWeight(.medium)
+                    .padding(.vertical, 8)
+                    .background(selectedSegment == segment ? Color.blue : Color.gray.opacity(0.1))
+                    .foregroundColor(selectedSegment == segment ? .white : .black)
+                    .cornerRadius(20)
+                    .onTapGesture {
+                        withAnimation {
+                            selectedSegment = segment
+                        }
+                    }
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .background(Color.gray.opacity(0.1))
+        .cornerRadius(20)
+        .padding(.horizontal, 0)
+    }
+}
+
+
+enum Segment: String, CaseIterable {
+    case favorites = "Favorites"
+    case downloads = "Downloads"
 }
