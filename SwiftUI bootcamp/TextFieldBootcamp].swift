aee40cae45e48e0926b0a5be05aa158dfcbaf5ab
MurodjonT <murodjon.mmm.99@mail.ru>
2025-05-24 19:27:40 +0500

    update confirmation dialog

---
+++ SwiftUI bootcamp/TextFieldBootcamp].swift
@@ -0,0 +1,18 @@
+//
+//  TextFieldBootcamp].swift
+//  SwiftUI bootcamp
+//
+//  Created by Murodjon Turobov on 24/05/25.
+//
+
+import SwiftUI
+
+struct TextFieldBootcamp_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextFieldBootcamp_()
+}
